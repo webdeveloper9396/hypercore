15ee516fa29901759422204196c9d137e377aed7
webdeveloper9396 <rahulkumarwebdeveloper9396@gmail.com>
2026-07-16 19:23:13 +0530

    Refactor Config class to use default export

--- src/core/Config.js
+++ src/core/Config.js
@@ -1,71 +1,541 @@
 /**
- * HyperCore Configuration
- * Global optimization settings
+ * HyperCore Configuration Manager
+ * Version: 1.0.0
  */
 
-export class Config {
+
+export default class Config {
 
 
-    constructor(options = {}) {
+
+    constructor(
+        userConfig = {}
+    ){
 
 
-        this.settings = {
 
-            // Engine
-            enabled: true,
-
-            debug: true,
+        this.defaults = {
 
 
-            // Images
-            optimizeImages: true,
-            lazyImages: true,
-            preloadLCP: true,
+            core: {
 
 
-            // CSS
-            optimizeCSS: true,
+                version:
+                "1.0.0",
 
 
-            // Javascript
-            optimizeJS: true,
+                debug:
+                true,
 
 
-            // Fonts
-            optimizeFonts: true,
+                safeMode:
+                true
 
 
-            // Safety
-            safeMode: true,
+
+            },
 
 
-            ...options
+
+
+
+            image: {
+
+
+                enabled:
+                true,
+
+
+                lazyLoad:
+                true,
+
+
+                fetchPriority:
+                true,
+
+
+                preload:
+                true
+
+
+
+            },
+
+
+
+
+
+
+
+            lcp: {
+
+
+                enabled:
+                true,
+
+
+                preload:
+                true,
+
+
+                fetchPriority:
+                true
+
+
+
+            },
+
+
+
+
+
+
+
+            css: {
+
+
+                enabled:
+                true,
+
+
+                preload:
+                true,
+
+
+                removeDuplicate:
+                true
+
+
+
+            },
+
+
+
+
+
+
+
+            javascript: {
+
+
+                enabled:
+                true,
+
+
+                delayThirdParty:
+                true,
+
+
+                delayTime:
+                3000
+
+
+
+            },
+
+
+
+
+
+
+
+            fonts: {
+
+
+                enabled:
+                true,
+
+
+                fontDisplaySwap:
+                true,
+
+
+                preload:
+                true
+
+
+
+            },
+
+
+
+
+
+
+
+            lazyLoader: {
+
+
+                enabled:
+                true,
+
+
+                rootMargin:
+                "200px"
+
+
+
+            },
+
+
+
+
+
+
+
+            analytics: {
+
+
+                enabled:
+                true,
+
+
+                delay:
+                true,
+
+
+                delayTime:
+                4000
+
+
+
+            },
+
+
+
+
+
+
+
+            dom: {
+
+
+                enabled:
+                true,
+
+
+                maxNodes:
+                1500
+
+
+
+            }
+
+
+
+
+
 
         };
 
 
+
+
+
+        this.config =
+            this.merge(
+
+                this.defaults,
+
+                userConfig
+
+            );
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+    /**
+     * Deep Merge
+     */
+
+
+    merge(
+        target,
+        source
+    ){
+
+
+
+        const output =
+            {
+                ...target
+            };
+
+
+
+
+
+
+        if(
+            this.isObject(target)
+            &&
+            this.isObject(source)
+        ){
+
+
+
+            Object.keys(source)
+            .forEach(key=>{
+
+
+
+
+
+                if(
+                    this.isObject(
+                        source[key]
+                    )
+                ){
+
+
+                    output[key] =
+                    this.merge(
+
+                        target[key] || {},
+
+                        source[key]
+
+                    );
+
+
+
+                }
+
+
+                else{
+
+
+                    output[key] =
+                    source[key];
+
+
+                }
+
+
+
+
+
+            });
+
+
+
+
+        }
+
+
+
+
+        return output;
+
+
+
     }
 
 
-    get(key) {
 
-        return this.settings[key];
+
+
+
+
+
+
+    isObject(
+        value
+    ){
+
+
+        return (
+
+            value &&
+            typeof value === "object" &&
+            !Array.isArray(value)
+
+        );
+
 
     }
 
 
-    set(key, value) {
 
-        this.settings[key] = value;
+
+
+
+
+
+
+    /**
+     * Get Config
+     */
+
+
+    get(
+        path
+    ){
+
+
+
+        return path
+        .split(".")
+        .reduce(
+
+            (obj,key)=>
+            obj?.[key],
+
+            this.config
+
+        );
+
+
 
     }
 
 
-    all() {
 
-        return this.settings;
+
+
+
+
+
+
+    /**
+     * Set Config
+     */
+
+
+    set(
+        path,
+        value
+    ){
+
+
+
+        const keys =
+            path.split(".");
+
+
+
+        let current =
+            this.config;
+
+
+
+
+
+        keys.forEach(
+            (key,index)=>{
+
+
+
+
+
+                if(
+                    index === keys.length - 1
+                ){
+
+
+
+                    current[key] =
+                    value;
+
+
+
+                }
+
+
+                else{
+
+
+
+                    if(
+                        !current[key]
+                    ){
+
+
+                        current[key] = {};
+
+                    }
+
+
+
+                    current =
+                    current[key];
+
+
+                }
+
+
+
+
+
+            }
+        );
+
+
+
+        return this;
+
 
     }
+
+
+
+
+
+
+
+
+
+    /**
+     * Get Full Config
+     */
+
+
+    all(){
+
+
+
+        return this.config;
+
+
+    }
+
+
+
+
+
+
+
+
+
+    /**
+     * Reset Config
+     */
+
+
+    reset(){
+
+
+
+        this.config =
+        structuredClone(
+            this.defaults
+        );
+
+
+
+    }
+
+
+
 
 
 }
