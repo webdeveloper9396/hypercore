aa61c9d58d77dc152f695a4715a966d3608fd3c6
webdeveloper9396 <rahulkumarwebdeveloper9396@gmail.com>
2026-07-16 18:11:00 +0530

    Register ImageOptimizer with HyperCore
    Added ImageOptimizer module registration to HyperCore.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@
 
 import { HyperCore } from "./core/Engine.js";
 
+import ImageOptimizer from "./modules/ImageOptimizer/index.js";
+
+
+// Create HyperCore instance
 
 const hypercore = new HyperCore({
 
@@ -16,7 +20,18 @@ const hypercore = new HyperCore({
 });
 
 
+// Register optimization modules
+
+hypercore.register(
+    ImageOptimizer
+);
+
+
+// Start Engine
+
 hypercore.init();
 
+
+// Export instance
 
 export default hypercore;
