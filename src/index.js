e00cb3825a5247a003391acddc6911903cd94030
webdeveloper9396 <rahulkumarwebdeveloper9396@gmail.com>
2026-07-16 17:53:08 +0530

    Initialize HyperCore in index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/**
+ * HyperCore
+ * Frontend Performance Optimization Engine
+ * Version: 1.0.0
+ */
+
+import { HyperCore } from "./core/Engine.js";
+
+const hypercore = new HyperCore();
+
+hypercore.init();
+
+export default hypercore;
